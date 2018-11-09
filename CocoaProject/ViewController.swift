86eee3b628069921015fee2120fb791bb9622b8f
thomastangerine <thomastang.thomas97@gmail.com>
2018-11-08 21:25:35 -0800

    Initial Commit

---
+++ CocoaProject/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  CocoaProject
+//
+//  Created by Thomas Tang on 11/8/18.
+//  Copyright © 2018 Thomas Tang. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
