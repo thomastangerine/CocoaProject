82bdc3d08f64cdcccb8dfb846597c3ae6904cb90
thomastangerine <thomastang.thomas97@gmail.com>
2018-11-20 13:57:26 -0800

    Initial commit

--- CocoaProject/ViewController.swift
+++ CocoaProject/ViewController.swift
@@ -7,14 +7,34 @@
 //
 
 import UIKit
+import PromiseKit
+import Alamofire
+import AlamofireSwiftyJSON
+import SwiftyJSON
 
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
+        firstly {
+            httpReq()
+        }.done { response in
+            print(response)
+        }
     }
 
+    func httpReq() -> Promise<JSON> {
+        return Promise { seal in
+            Alamofire.request("https://api.foursquare.com/v2/venues/search?client_id=4TCOBRNYJSHUVUA21XNC1NGYF1EFSMKSSCPTLPHXE1KITWME&client_secret=M4JZCLSXBFPDKS0G52OYPI1J13KIYDIYKLS5J0ASRCDKDVTE&near=La%20Jolla%2C%20CA&categoryId=4bf58dd8d48988d118951735&v=20180101&limit=1&fbclid=IwAR3TTbsx6_gXdT9qyWnxaDK1_EDQl8gVziopCccnmEVMFxj-RgqQw1yxoyY").responseSwiftyJSON { response in
+                if let data = response.result.value{
+                    seal.fulfill(data)
+                }
+            }
+        }
+    }
+    
+    func 
 
 }
 
---
+++ Podfile
@@ -0,0 +1,10 @@
+use_frameworks!
+
+target 'CocoaProject' do
+  pod 'AFNetworking', '~> 3.0'
+  pod 'FBSDKCoreKit', '~> 4.9'
+  pod 'PromiseKit', '~> 6.0'
+  pod 'Alamofire', '~> 4.7'
+  pod 'AlamofireSwiftyJSON'
+  
+end
